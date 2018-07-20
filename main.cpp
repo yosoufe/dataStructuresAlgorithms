2bd9b80dc4579246629bfeaa51178121046b726d
Yousof <yousofebneddin@yahoo.com>
2018-07-19 20:00:45 -0700

    standard c++ heap

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "binaryheap.h"
+#include <algorithm>
+#include <random>
 
 using namespace std;
 
@@ -11,5 +13,30 @@ int main()
   hp.insert(15);
   hp.insert(8);
   cout << hp.extractMax() << endl;
+
+  // c++ standard heap
+  vector<float> vec;
+  for (int i = 0; i <10 ; i++){
+    vec.push_back(float(rand())/RAND_MAX);
+    cout << vec.back() << ", ";
+  }
+  cout << endl << "---after heap---" << endl;
+  make_heap(vec.begin(),vec.end());
+
+  for (unsigned int i = 0; i <10 ; i++){
+    cout << vec[i] << ", ";
+  }
+  cout << endl << "--- sort ---" << endl;
+
+  for (int i = 0; i <10 ; i++){
+    cout << vec.front() << ", ";
+    pop_heap(vec.begin(),vec.end());
+    vec.pop_back();
+  }
+  cout << endl;
+
+  // pop_heap // push_heap
+
+
   return 0;
 }
